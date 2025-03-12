faf85f2e9696cc2d9453fb0d854cc97cc55ef088
Dakomos <leonardschmitt98@gmail.com>
2025-03-12 14:42:23 +0100

    Commented code and formatted

--- hdc2010.c
+++ hdc2010.c
@@ -8,116 +8,176 @@
  *
  */
 
-#include "hdc2010.h"
-#include "stm32l4xx_hal.h"
-
-uint8_t HDC2010_Init(HDC2010 *dev, I2C_HandleTypeDef *i2cHandle)
-{
-
-    dev->i2cHandle = i2cHandle;
-
-    dev->temp_C = 0.0f;
-
-    dev->humid = 0.0f;
-
-    /* Store number of errors. If this initialisation returns > 0, then it's not initialised correctly */
-    uint8_t errNum = 0;
-    HAL_StatusTypeDef status;
-
-    uint8_t regData[2];
-    /* Check device and part IDs ( Datasheet page 17 ) */
-
-    /* Check device ID */
-    status = HDC2010_ReadRegister(dev, HDC2010_DEV_ID_HIGH_REG, &regData[0]);
-    status = HDC2010_ReadRegister(dev, HDC2010_DEV_ID_LOW_REG, &regData[1]);
-    errNum += (status != HAL_OK);
-
-    if (((regData[0] << 8) | regData[1]) != HDC2010_DEV_ID)
-    {
-
-        return 255;
-    }
-
-    /* Check manufacturer ID */
-    status = HDC2010_ReadRegister(dev, HDC2010_MANU_ID_HIGH_REG, &regData[1]); // switch because our registers seem to be switched
-    status = HDC2010_ReadRegister(dev, HDC2010_MANU_ID_LOW_REG, &regData[0]);
-    errNum += (status != HAL_OK);
-
-    if (((regData[0] << 8) | regData[1]) != HDC2010_MANU_ID)
-    {
-
-        return 255;
-    }
-
-    /* Start measurement ( Data sheet page 26 )*/
-    regData[0] = 0x01;
-
-    status = HDC2010_WriteRegister(dev, HDC2010_MEASURE_CONF_REG, &regData[0]);
-    errNum += (status != HAL_OK);
-
-    return errNum;
-}
-
-/*
- *   Data
- */
-
-HAL_StatusTypeDef HDC2010_ReadTemp(HDC2010 *dev)
-{
-
-    uint8_t temp[2];
-    HAL_StatusTypeDef status;
-
-    status = HDC2010_ReadRegister(dev, HDC2010_TEMP_LOW_REG, temp[0]);
-    if (status != HAL_OK)
-        return status;
-
-    status = HDC2010_ReadRegister(dev, HDC2010_TEMP_HIGH_REG, temp[1]);
-    if (status != HAL_OK)
-        return status;
-
-    dev->temp_C = ((((temp[0] << 8) | temp[1]) * 165) / 65536) - 40;
-
-    return status;
-}
-
-HAL_StatusTypeDef HDC2010_ReadHum(HDC2010 *dev)
-{
-
-    uint8_t humid[2];
-    HAL_StatusTypeDef status;
-
-    status = HDC2010_ReadRegister(dev, HDC2010_HUM_LOW_REG, humid[0]);
-    if (status != HAL_OK)
-        return status;
-
-    status = HDC2010_ReadRegister(dev, HDC2010_HUM_HIGH_REG, humid[1]);
-    if (status != HAL_OK)
-        return status;
-
-    dev->humid = ((((humid[0] << 8) | humid[1]) * 100) / 65536);
-
-    return status;
-}
-
-/*
- *   Low Level
- */
-
-HAL_StatusTypeDef HDC2010_ReadRegister(HDC2010 *dev, uint8_t reg, uint8_t *data)
-{
-
-    return HAL_I2C_Mem_Read(dev->i2cHandle, HDC2010_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
-}
-
-HAL_StatusTypeDef HDC2010_ReadRegisters(HDC2010 *dev, uint8_t reg, uint8_t *data, uint8_t length)
-{
-
-    return HAL_I2C_Mem_Read(dev->i2cHandle, HDC2010_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, length, HAL_MAX_DELAY);
-}
-
-HAL_StatusTypeDef HDC2010_WriteRegister(HDC2010 *dev, uint8_t reg, uint8_t *data)
-{
-
-    return HAL_I2C_Mem_Write(dev->i2cHandle, HDC2010_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
-}
+ #include "hdc2010.h"
+ #include "stm32l4xx_hal.h"
+ 
+ /**
+  * @brief  Initializes the HDC2010 sensor.
+  * 
+  * Diese Funktion initialisiert den HDC2010-Sensor und überprüft die Geräte- und Hersteller-IDs.
+  * 
+  * @param  dev         Pointer auf die HDC2010-Struktur.
+  * @param  i2cHandle   Pointer auf die I2C-Handle-Struktur.
+  * @retval uint8_t     Anzahl der Fehler während der Initialisierung. Wenn > 0, dann ist die Initialisierung fehlgeschlagen.
+  */
+ uint8_t HDC2010_Init(HDC2010 *dev, I2C_HandleTypeDef *i2cHandle)
+ {
+     dev->i2cHandle = i2cHandle;
+     dev->temp_C = 0.0f;
+     dev->humid = 0.0f;
+ 
+     uint8_t errNum = 0;
+     HAL_StatusTypeDef status;
+     uint8_t regData[2];
+ 
+     /* Check device and part IDs ( Datasheet page 17 ) */
+ 
+     /* Check device ID */
+     status = HDC2010_ReadRegister(dev, HDC2010_DEV_ID_HIGH_REG, &regData[0]);
+     status = HDC2010_ReadRegister(dev, HDC2010_DEV_ID_LOW_REG, &regData[1]);
+     errNum += (status != HAL_OK);
+ 
+     if (((regData[0] << 8) | regData[1]) != HDC2010_DEV_ID)
+     {
+         return 255;
+     }
+ 
+     /* Check manufacturer ID */
+     status = HDC2010_ReadRegister(dev, HDC2010_MANU_ID_HIGH_REG, &regData[1]); // switch because our registers seem to be switched
+     status = HDC2010_ReadRegister(dev, HDC2010_MANU_ID_LOW_REG, &regData[0]);
+     errNum += (status != HAL_OK);
+ 
+     if (((regData[0] << 8) | regData[1]) != HDC2010_MANU_ID)
+     {
+         return 255;
+     }
+ 
+     /* Start measurement ( Data sheet page 26 ) */
+ 
+     return errNum;
+ }
+ 
+ /**
+  * @brief  Starts a measurement in one-shot mode.
+  * 
+  * Diese Funktion startet eine Messung im One-Shot-Modus. Die Messung wird in den Registern des Geräts gespeichert.
+  * 
+  * @param  dev         Pointer auf die HDC2010-Struktur.
+  * @retval HAL_StatusTypeDef  Status der HAL-Funktion. HAL_OK bei Erfolg, sonst Fehlercode.
+  */
+ HAL_StatusTypeDef HDC2010_StartMeasurement(HDC2010 *dev)
+ {
+     uint8_t regData[2];
+     regData[0] = 0x01;
+ 
+     uint8_t errNum = 0;
+     HAL_StatusTypeDef status;
+ 
+     status = HDC2010_WriteRegister(dev, HDC2010_MEASURE_CONF_REG, &regData[0]);
+     errNum += (status != HAL_OK);
+ 
+     return errNum;
+ }
+ 
+ /**
+  * @brief  Reads the temperature from the HDC2010 sensor.
+  * 
+  * Diese Funktion liest die Temperatur vom HDC2010-Sensor und speichert sie in der Struktur.
+  * 
+  * @param  dev         Pointer auf die HDC2010-Struktur.
+  * @retval HAL_StatusTypeDef  Status der HAL-Funktion. HAL_OK bei Erfolg, sonst Fehlercode.
+  */
+ HAL_StatusTypeDef HDC2010_ReadTemp(HDC2010 *dev)
+ {
+     uint8_t temp[2];
+     HAL_StatusTypeDef status;
+ 
+     HDC2010_StartMeasurement(dev);
+ 
+     status = HDC2010_ReadRegister(dev, HDC2010_TEMP_LOW_REG, &temp[1]);
+     if (status != HAL_OK)
+         return status;
+ 
+     status = HDC2010_ReadRegister(dev, HDC2010_TEMP_HIGH_REG, &temp[0]);
+     if (status != HAL_OK)
+         return status;
+ 
+     dev->temp_C = ((((temp[0] << 8) | temp[1]) * 165.0) / 65536) - 40;
+ 
+     return status;
+ }
+ 
+ /**
+  * @brief  Reads the humidity from the HDC2010 sensor.
+  * 
+  * Diese Funktion liest die Luftfeuchtigkeit vom HDC2010-Sensor und speichert sie in der Struktur.
+  * 
+  * @param  dev         Pointer auf die HDC2010-Struktur.
+  * @retval HAL_StatusTypeDef  Status der HAL-Funktion. HAL_OK bei Erfolg, sonst Fehlercode.
+  */
+ HAL_StatusTypeDef HDC2010_ReadHum(HDC2010 *dev)
+ {
+     uint8_t humid[2];
+     HAL_StatusTypeDef status;
+ 
+     HDC2010_StartMeasurement(dev);
+ 
+     status = HDC2010_ReadRegister(dev, HDC2010_HUM_LOW_REG, &humid[1]);
+     if (status != HAL_OK)
+         return status;
+ 
+     status = HDC2010_ReadRegister(dev, HDC2010_HUM_HIGH_REG, &humid[0]);
+     if (status != HAL_OK)
+         return status;
+ 
+     dev->humid = ((((humid[0] << 8) | humid[1]) * 100.0) / 65536);
+ 
+     return status;
+ }
+ 
+ /**
+  * @brief  Reads a register from the HDC2010 sensor.
+  * 
+  * Diese Funktion liest ein Register vom HDC2010-Sensor.
+  * 
+  * @param  dev         Pointer auf die HDC2010-Struktur.
+  * @param  reg         Registeradresse, die gelesen werden soll.
+  * @param  data        Pointer auf den Speicher, in dem die gelesenen Daten gespeichert werden.
+  * @retval HAL_StatusTypeDef  Status der HAL-Funktion. HAL_OK bei Erfolg, sonst Fehlercode.
+  */
+ HAL_StatusTypeDef HDC2010_ReadRegister(HDC2010 *dev, uint8_t reg, uint8_t *data)
+ {
+     return HAL_I2C_Mem_Read(dev->i2cHandle, HDC2010_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
+ }
+ 
+ /**
+  * @brief  Reads multiple registers from the HDC2010 sensor.
+  * 
+  * Diese Funktion liest mehrere Register vom HDC2010-Sensor.
+  * 
+  * @param  dev         Pointer auf die HDC2010-Struktur.
+  * @param  reg         Startadresse des ersten Registers, das gelesen werden soll.
+  * @param  data        Pointer auf den Speicher, in dem die gelesenen Daten gespeichert werden.
+  * @param  length      Anzahl der zu lesenden Register.
+  * @retval HAL_StatusTypeDef  Status der HAL-Funktion. HAL_OK bei Erfolg, sonst Fehlercode.
+  */
+ HAL_StatusTypeDef HDC2010_ReadRegisters(HDC2010 *dev, uint8_t reg, uint8_t *data, uint8_t length)
+ {
+     return HAL_I2C_Mem_Read(dev->i2cHandle, HDC2010_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, length, HAL_MAX_DELAY);
+ }
+ 
+ /**
+  * @brief  Writes a register to the HDC2010 sensor.
+  * 
+  * Diese Funktion schreibt ein Register des HDC2010-Sensors.
+  * 
+  * @param  dev         Pointer auf die HDC2010-Struktur.
+  * @param  reg         Registeradresse, die geschrieben werden soll.
+  * @param  data        Pointer auf den Speicher, der die zu schreibenden Daten enthält.
+  * @retval HAL_StatusTypeDef  Status der HAL-Funktion. HAL_OK bei Erfolg, sonst Fehlercode.
+  */
+ HAL_StatusTypeDef HDC2010_WriteRegister(HDC2010 *dev, uint8_t reg, uint8_t *data)
+ {
+     return HAL_I2C_Mem_Write(dev->i2cHandle, HDC2010_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
+ }
+ 
--- hdc2010.h
+++ hdc2010.h
@@ -8,82 +8,83 @@
  *
  */
 
-#include "stm32l4xx_hal.h" // Needed for I2C
-#include <stdint.h>
-
-#ifndef HDC2010_I2C_DRIVER_H
-#define HDC2010_I2C_DRIVER_H
-
-/*
- *   DEFINES
- */
-
-#define HDC2010_I2C_ADDR (0x40 << 1) // 1000000 in BIN, Seite 3 Datasheet
-
-/*
- *   REGISTERS
- */
-
-#define HDC2010_TEMP_LOW_REG 0x00  // Data sheet page 18
-#define HDC2010_TEMP_HIGH_REG 0x01 // Temperatur is a 14 bit integer stored in two registers. The two LSB are always 0
-#define HDC2010_HUM_LOW_REG 0x02   // Data sheet page 18
-#define HDC2010_HUM_HIGH_REG 0x03  // Humidity is a 14 bit integer stored in two registers. The two LSB are always 0
-#define HDC2010_INTERRUPT_REG 0x04
-#define HDC2010_TEMP_MAX_REG 0x05
-#define HDC2010_HUM_MAX_REG 0x06
-#define HDC2010_INTERRUPT_ENABLE_REG 0x07
-#define HDC2010_TEMP_OFFSET_REG 0x08
-#define HDC2010_HUM_OFFSET_REG 0x09
-#define HDC2010_TEMP_THR_LOW_REG 0x0A
-#define HDC2010_TEMP_THR_HIGH_REG 0x0B
-#define HDC2010_RH_THR_LOW_REG 0x0C
-#define HDC2010_RH_THR_HIGH_REG 0x0D
-#define HDC2010_RESET_CONF_REG 0x0E   // Data sheet page 25. Settings for auto measurement modes
-#define HDC2010_MEASURE_CONF_REG 0x0F // Data sheet page 26. Setting for standby or measurement. Setting for precision of temperatur and humidty integer
-#define HDC2010_MANU_ID_LOW_REG 0xFC
-#define HDC2010_MANU_ID_HIGH_REG 0xFD
-#define HDC2010_DEV_ID_LOW_REG 0xFE
-#define HDC2010_DEV_ID_HIGH_REG 0xFF
-
-#define HDC2010_MANU_ID 0x4954 // Data sheet page 26
-#define HDC2010_DEV_ID 0x07D0  // Data sheet page 27
-
-/*
- *   SENSOR STRUCT
- */
-
-typedef struct
-{
-    /* I2C handle */
-    I2C_HandleTypeDef *i2cHandle;
-
-    /* Temperatur Data */
-    float temp_C;
-
-    /* Humidity Data */
-    float humid;
-} HDC2010;
-
-/*
- *   Init
- */
-
-uint8_t HDC2010_Init(HDC2010 *dev, I2C_HandleTypeDef *i2cHandle);
-
-/*
- *   Data
- */
-
-HAL_StatusTypeDef HDC2010_ReadTemp(HDC2010 *dev);
-HAL_StatusTypeDef HDC2010_ReadHum(HDC2010 *dev);
-
-/*
- *   Low Level
- */
-
-HAL_StatusTypeDef HDC2010_ReadRegister(HDC2010 *dev, uint8_t reg, uint8_t *data);
-HAL_StatusTypeDef HDC2010_ReadRegisters(HDC2010 *dev, uint8_t reg, uint8_t *data, uint8_t length);
-
-HAL_StatusTypeDef HDC2010_WriteRegister(HDC2010 *dev, uint8_t reg, uint8_t *data);
-
-#endif
+ #include "stm32l4xx_hal.h" // Needed for I2C
+ #include <stdint.h>
+ 
+ #ifndef HDC2010_I2C_DRIVER_H
+ #define HDC2010_I2C_DRIVER_H
+ 
+ /*
+  *   DEFINES
+  */
+ 
+ #define HDC2010_I2C_ADDR (0x40 << 1) // 1000000 in BIN, Seite 3 Datasheet
+ 
+ /*
+  *   REGISTERS
+  */
+ 
+ #define HDC2010_TEMP_LOW_REG 0x00  // Data sheet page 18
+ #define HDC2010_TEMP_HIGH_REG 0x01 // Temperatur is a 14 bit integer stored in two registers. The two LSB are always 0
+ #define HDC2010_HUM_LOW_REG 0x02   // Data sheet page 18
+ #define HDC2010_HUM_HIGH_REG 0x03  // Humidity is a 14 bit integer stored in two registers. The two LSB are always 0
+ #define HDC2010_INTERRUPT_REG 0x04
+ #define HDC2010_TEMP_MAX_REG 0x05
+ #define HDC2010_HUM_MAX_REG 0x06
+ #define HDC2010_INTERRUPT_ENABLE_REG 0x07
+ #define HDC2010_TEMP_OFFSET_REG 0x08
+ #define HDC2010_HUM_OFFSET_REG 0x09
+ #define HDC2010_TEMP_THR_LOW_REG 0x0A
+ #define HDC2010_TEMP_THR_HIGH_REG 0x0B
+ #define HDC2010_RH_THR_LOW_REG 0x0C
+ #define HDC2010_RH_THR_HIGH_REG 0x0D
+ #define HDC2010_RESET_CONF_REG 0x0E   // Data sheet page 25. Settings for auto measurement modes
+ #define HDC2010_MEASURE_CONF_REG 0x0F // Data sheet page 26. Setting for standby or measurement. Setting for precision of temperatur and humidty integer
+ #define HDC2010_MANU_ID_LOW_REG 0xFC
+ #define HDC2010_MANU_ID_HIGH_REG 0xFD
+ #define HDC2010_DEV_ID_LOW_REG 0xFE
+ #define HDC2010_DEV_ID_HIGH_REG 0xFF
+ 
+ #define HDC2010_MANU_ID 0x4954 // Data sheet page 26
+ #define HDC2010_DEV_ID 0x07D0  // Data sheet page 27
+ 
+ /*
+  *   SENSOR STRUCT
+  */
+ 
+ typedef struct
+ {
+     /* I2C handle */
+     I2C_HandleTypeDef *i2cHandle;
+ 
+     /* Temperatur Data */
+     float temp_C;
+ 
+     /* Humidity Data */
+     float humid;
+ } HDC2010;
+ 
+ /*
+  *   Init
+  */
+ 
+ uint8_t HDC2010_Init(HDC2010 *dev, I2C_HandleTypeDef *i2cHandle);
+ 
+ /*
+  *   Data
+  */
+ 
+ HAL_StatusTypeDef HDC2010_ReadTemp(HDC2010 *dev);
+ HAL_StatusTypeDef HDC2010_ReadHum(HDC2010 *dev);
+ 
+ /*
+  *   Low Level
+  */
+ 
+ HAL_StatusTypeDef HDC2010_ReadRegister(HDC2010 *dev, uint8_t reg, uint8_t *data);
+ HAL_StatusTypeDef HDC2010_ReadRegisters(HDC2010 *dev, uint8_t reg, uint8_t *data, uint8_t length);
+ 
+ HAL_StatusTypeDef HDC2010_WriteRegister(HDC2010 *dev, uint8_t reg, uint8_t *data);
+ 
+ #endif
+ 
